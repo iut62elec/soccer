a883e339c03632f66cdf120e29926e6c7afcae8b
jpedram <jpedram@amazon.com>
2023-03-26 19:08:35 -0400

    3rd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
   const [inputData, setInputData] = useState({ file_name: "" });
+  const [showVideo, setShowVideo] = useState(false);
 
   const handleInputChange = (e) => {
     setInputData({ file_name: e.target.value });
@@ -20,10 +21,22 @@ function App() {
       console.error(error);
     }
   };
+  useEffect(() => {
+    // Replace this with your backend API call to get notification when process finishes
+    const checkProcessStatus = async () => {
+      const response = await fetch('/api/process-status');
+      const data = await response.json();
+      if (data.status === 'finished') {
+        setShowVideo(true);
+      }
+    };
+    //setShowVideo(true)
+    checkProcessStatus();
+  }, []);
 
   return (
     <div>
-      <h1>Trigger AWS Step Function with Input Data</h1>
+      <h1>Input Video File name and hit submit!</h1>
       <form onSubmit={handleFormSubmit}>
         <label>
           File Name:
@@ -33,9 +46,15 @@ function App() {
             onChange={handleInputChange}
           />
         </label>
-        <br />
+        
         <button type="submit">Submit</button>
       </form>
+    <br></br>
+      {showVideo ? (
+        <video src="https://d35nrjxlam6z0e.cloudfront.net/videoplayback_iran-from-13-to-16HL.mp4" controls />
+      ) : (
+        <p>Waiting for process to finish...</p>
+      )}
     </div>
   );
 }
