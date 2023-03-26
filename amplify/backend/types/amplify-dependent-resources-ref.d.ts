53a3b81badc8b5f6d0d87dfa4e24a056357fe6e6
jpedram <jpedram@amazon.com>
2023-03-26 17:05:05 -0400

    2nd commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "soccer": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "SoccerStartSF": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [inputData, setInputData] = useState({ file_name: "" });
+
+  const handleInputChange = (e) => {
+    setInputData({ file_name: e.target.value });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://qnnyj5ljke.execute-api.us-east-1.amazonaws.com/dev/SoccerStartSF-dev",
+        inputData
+      );
+      console.log(response.data); // Response from the backend API
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Trigger AWS Step Function with Input Data</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          File Name:
+          <input
+            type="text"
+            value={inputData.file_name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
